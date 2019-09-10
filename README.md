5dc227930e02a876db09b294978e0c856d3104e4
Leon Stigter <lstigter@gmail.com>
2019-09-10 11:47:40 -0700

    Update with TLS option (#1)
    * Update main.go
    
    * Update README.md
    
    * Update LICENSE

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 # httpserver
+
 A simple httpserver in the idea of `python -m SimpleHTTPServer`
 
 ## usage
-```
-$ httpserver [--port] [--root]
+
+```go
+$ httpserver [-port] [-dir] [-tlscert] [-tlskey]
 ```
 
 * port is the HTTP port that will be used (defaults to `8080`)
-* root is the root directory (defaults to current dir `.`)
+* dir is the root directory (defaults to current dir `.`)
+* tlscert is the name of the TLS certificate file
+* tlskey is the name of the TLS key file
+
+Using all parameters, the command would be
+
+```go
+$ httpserver -port 8080 -dir . -tlscert local.pem -tlskey local-key.pem
+```
+
+If you're looking for a simple zero-config tool to make locally trusted development certificates with any names you'd like, check out [mkcert](https://github.com/FiloSottile/mkcert)
 
 ## install
-```
+
+```go
 $ go get -u github.com/retgits/httpserver
 ```
 
 ## thanks
+
 The code is originally from [this](https://www.chrismytton.uk/2013/07/17/golang-static-http-file-server/) blog
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 // Package main implements a simple httpserver in the idea of
-// `python -m SimpleHTTPServer`. Flags:
+// `python -m SimpleHTTPServer`.
+//
+// Flags:
 // port is the HTTP port that will be used (defaults to `8080`)
-// root is the root directory (defaults to current dir `.`)
+// dir is the root directory (defaults to current dir `.`)
+// tlscert is the name of the TLS certificate file
+// tlskey is the name of the TLS key file
+//
 // Code originally from https://www.chrismytton.uk/2013/07/17/golang-static-http-file-server/
 package main
 
@@ -9,14 +14,32 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"path/filepath"
 )
 
-var port = flag.String("port", "8080", "Define what TCP port to bind to")
-var root = flag.String("root", ".", "Define the root filesystem path")
+var (
+	port    = flag.String("port", "8080", "TCP port (defaults to 8080)")
+	dir     = flag.String("dir", ".", "Filesystem directory to serve (defaults to current dir)")
+	tlscert = flag.String("tlscert", "", "Name of the TLS certificate file")
+	tlskey  = flag.String("tlskey", "", "Name of the TLS key file")
+)
 
 func main() {
+	// Parse flags
 	flag.Parse()
-	fmt.Printf("Starting HTTP server on %s\n", *port)
-	fmt.Printf("Serving up %s\n", *root)
-	panic(http.ListenAndServe(":"+*port, http.FileServer(http.Dir(*root))))
+
+	// Get the correct directory name
+	folder, err := filepath.Abs(filepath.Dir(*dir))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("HTTP Server\nPort     : %s\nDirectory: %s\n", *port, folder)
+
+	if len(*tlscert) == 0 {
+		fmt.Printf("\nYou're not using HTTPS... You can create selfsigned and trusted certificates for your machine\nusing mkcert: https://github.com/FiloSottile/mkcert")
+		panic(http.ListenAndServe(":"+*port, http.FileServer(http.Dir(folder))))
+	}
+
+	panic(http.ListenAndServeTLS(":"+*port, *tlscert, *tlskey, http.FileServer(http.Dir(folder))))
 }
